dce1ac12b4b1a4d5142685bd6588c96316687f3f
RadonCoding <86915746+RadonCoding@users.noreply.github.com>
2026-05-28 16:46:57 +0300

    Third commit

--- runtime/src/vm/encoders/jcc.rs
+++ runtime/src/vm/encoders/jcc.rs
@@ -1,4 +1,5 @@
 use rand::seq::SliceRandom;
+use rand::Rng;
 
 use crate::mapper::{mapped, Mapper};
 use crate::vm::bytecode::{VMOp, VMReg};
@@ -39,10 +40,86 @@ pub struct Jcc {
     pub destination: u32,
 }
 
+impl VMCondition {
+    fn swap(&self) -> Self {
+        Self {
+            test: self.test,
+            lhs: self.rhs,
+            rhs: self.lhs,
+        }
+    }
+}
+
+impl Jcc {
+    fn mutate(&mut self) {
+        let mut rng = rand::thread_rng();
+
+        #[cfg(debug_assertions)]
+        {
+            eprintln!("before:");
+            eprintln!("  logic={:?}", self.logic);
+            eprintln!("  conditions={:#?}", self.conditions);
+        }
+
+        self.conditions.shuffle(&mut rng);
+
+        for condition in &mut self.conditions {
+            if rng.gen() {
+                *condition = condition.swap();
+            }
+        }
+
+        let original = self.conditions.clone();
+
+        match rng.gen_range(0..6) {
+            0 => {}
+            1 => self.conditions.extend(original),
+            2 => self
+                .conditions
+                .extend(original.iter().map(VMCondition::swap)),
+            3 => self.logic = VMLogic::AND,
+            4 => self.logic = VMLogic::OR,
+            _ => {
+                self.conditions = self
+                    .conditions
+                    .iter()
+                    .flat_map(|condition| match condition.test {
+                        VMTest::NEQ => {
+                            self.logic = VMLogic::OR;
+
+                            vec![
+                                VMCondition {
+                                    test: VMTest::CMP,
+                                    lhs: condition.lhs,
+                                    rhs: condition.rhs,
+                                },
+                                VMCondition {
+                                    test: VMTest::CMP,
+                                    lhs: condition.rhs,
+                                    rhs: condition.lhs,
+                                },
+                            ]
+                        }
+
+                        _ => vec![condition.clone()],
+                    })
+                    .collect();
+            }
+        }
+
+        self.conditions.shuffle(&mut rng);
+
+        #[cfg(debug_assertions)]
+        {
+            eprintln!("after:");
+            eprintln!("  logic={:?}", self.logic);
+            eprintln!("  conditions={:#?}", self.conditions);
+        }
+    }
+}
 impl Encode for Jcc {
     fn encode(&mut self, mapper: &mut Mapper) -> Vec<u8> {
-        let mut rng = rand::thread_rng();
-        self.conditions.shuffle(&mut rng);
+        self.mutate();
 
         let mut bytes = vec![
             mapper.index(VMOp::Jcc),
