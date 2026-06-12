e6c0924990e1e38148b215aec496d058b4df3194
RadonCoding <86915746+RadonCoding@users.noreply.github.com>
2026-06-12 20:46:23 +0300

    fix typo

--- runtime/src/assembler.rs
+++ runtime/src/assembler.rs
@@ -42,7 +42,6 @@ fn scratch64(exclude: &[Register]) -> AsmRegister64 {
     const CANDIDATES: [AsmRegister64; 14] = [
         rax, rbx, rcx, rdx, rsi, rdi, r8, r9, r10, r11, r12, r13, r14, r15,
     ];
-
     *CANDIDATES
         .iter()
         .filter(|&&r| !exclude.contains(&r.into()))
@@ -54,7 +53,6 @@ fn scratch32(exclude: &[Register]) -> AsmRegister32 {
     const CANDIDATES: [AsmRegister32; 14] = [
         eax, ebx, ecx, edx, esi, edi, r8d, r9d, r10d, r11d, r12d, r13d, r14d, r15d,
     ];
-
     *CANDIDATES
         .iter()
         .filter(|&&r| !exclude.contains(&r.into()))
@@ -66,7 +64,6 @@ fn scratch16(exclude: &[Register]) -> AsmRegister16 {
     const CANDIDATES: [AsmRegister16; 14] = [
         ax, bx, cx, dx, si, di, r8w, r9w, r10w, r11w, r12w, r13w, r14w, r15w,
     ];
-
     *CANDIDATES
         .iter()
         .filter(|&&r| !exclude.contains(&r.into()))
@@ -79,7 +76,6 @@ fn scratch8(exclude: &[Register]) -> AsmRegister8 {
         al, cl, dl, bl, ah, ch, dh, bh, spl, bpl, sil, dil, r8b, r9b, r10b, r11b, r12b, r13b, r14b,
         r15b,
     ];
-
     *CANDIDATES
         .iter()
         .filter(|&&r| !exclude.contains(&r.into()))
@@ -91,28 +87,22 @@ fn imm() -> i32 {
     rand::random::<i32>() & 0xF
 }
 
-macro_rules! mba_prologue {
-    ($asm:expr, $tmp:expr) => {{
+macro_rules! operation {
+    ($asm:expr, $tmp:expr, $body:expr) => {{
         use iced_x86::code_asm::ptr;
 
         $asm.sub(rsp, 8)?;
         $asm.mov(ptr(rsp), $tmp)?;
-    }};
-}
 
-macro_rules! mba_epilogue {
-    ($asm:expr, $tmp:expr) => {{
-        use iced_x86::code_asm::ptr;
+        $body?;
 
         $asm.mov($tmp, ptr(rsp))?;
         $asm.add(rsp, 8)
     }};
 }
 
-macro_rules! mba_add {
+macro_rules! mba_add_reg {
     ($asm:expr, $dst:expr, $src:expr, $tmp:expr) => {{
-        mba_prologue!($asm, $tmp);
-
         match rand::random::<u8>() % 3 {
             0 => {
                 // a + b = (a & b) + (a | b)
@@ -141,15 +131,12 @@ macro_rules! mba_add {
                 $asm.sub($dst, k)?;
             }
         }
-
-        mba_epilogue!($asm, $tmp)
+        Ok::<(), IcedError>(())
     }};
 }
 
-macro_rules! mba_sub {
+macro_rules! mba_sub_reg {
     ($asm:expr, $dst:expr, $src:expr, $tmp:expr) => {{
-        mba_prologue!($asm, $tmp);
-
         match rand::random::<u8>() % 3 {
             0 => {
                 // a - b = a + (~b + 1)
@@ -176,8 +163,7 @@ macro_rules! mba_sub {
                 $asm.mov($dst, $tmp)?;
             }
         }
-
-        mba_epilogue!($asm, $tmp)
+        Ok::<(), IcedError>(())
     }};
 }
 
@@ -187,7 +173,6 @@ macro_rules! passthrough {
             fn add(self, dst: $dst, asm: &mut CodeAssembler) -> Result<(), IcedError> {
                 asm.add(dst, self)
             }
-
             fn sub(self, dst: $dst, asm: &mut CodeAssembler) -> Result<(), IcedError> {
                 asm.sub(dst, self)
             }
@@ -195,68 +180,68 @@ macro_rules! passthrough {
     };
 }
 
-passthrough!(AsmRegister8, i32);
-passthrough!(AsmRegister8, u32);
 passthrough!(AsmMemoryOperand, AsmRegister8);
-
-passthrough!(AsmRegister16, i32);
-passthrough!(AsmRegister16, u32);
 passthrough!(AsmMemoryOperand, AsmRegister16);
-
-passthrough!(AsmRegister32, i32);
-passthrough!(AsmRegister32, u32);
 passthrough!(AsmMemoryOperand, AsmRegister32);
-
-passthrough!(AsmRegister64, i32);
-
 passthrough!(AsmMemoryOperand, AsmRegister64);
-passthrough!(AsmMemoryOperand, i32);
-passthrough!(AsmMemoryOperand, u32);
 
-impl Obfuscate<AsmRegister64> for AsmRegister64 {
-    fn add(self, dst: AsmRegister64, asm: &mut CodeAssembler) -> Result<(), IcedError> {
-        let tmp = scratch64(&[dst.into(), self.into()]);
-        mba_add!(asm, dst, self, tmp)
+trait Load<Dst> {
+    fn load(self, asm: &mut CodeAssembler, dst: Dst) -> Result<(), IcedError>;
+}
+
+impl Load<AsmRegister64> for i32 {
+    fn load(self, asm: &mut CodeAssembler, dst: AsmRegister64) -> Result<(), IcedError> {
+        asm.mov(dst, self as i64)
     }
-
-    fn sub(self, dst: AsmRegister64, asm: &mut CodeAssembler) -> Result<(), IcedError> {
-        let tmp = scratch64(&[dst.into(), self.into()]);
-        mba_sub!(asm, dst, self, tmp)
+}
+impl Load<AsmRegister32> for i32 {
+    fn load(self, asm: &mut CodeAssembler, dst: AsmRegister32) -> Result<(), IcedError> {
+        asm.mov(dst, self)
+    }
+}
+impl Load<AsmRegister16> for i32 {
+    fn load(self, asm: &mut CodeAssembler, dst: AsmRegister16) -> Result<(), IcedError> {
+        asm.mov(dst, self)
+    }
+}
+impl Load<AsmRegister8> for i32 {
+    fn load(self, asm: &mut CodeAssembler, dst: AsmRegister8) -> Result<(), IcedError> {
+        asm.mov(dst, self)
     }
 }
 
-impl Obfuscate<AsmRegister32> for AsmRegister32 {
-    fn add(self, dst: AsmRegister32, asm: &mut CodeAssembler) -> Result<(), IcedError> {
-        let tmp = scratch32(&[dst.into(), self.into()]);
-        mba_add!(asm, dst, self, tmp)
-    }
-
-    fn sub(self, dst: AsmRegister32, asm: &mut CodeAssembler) -> Result<(), IcedError> {
-        let tmp = scratch32(&[dst.into(), self.into()]);
-        mba_sub!(asm, dst, self, tmp)
-    }
+macro_rules! impl_obfuscate {
+    ($dst:ty, $scratch:expr, $add:ident, $sub:ident) => {
+        impl Obfuscate<$dst> for $dst {
+            fn add(self, dst: $dst, asm: &mut CodeAssembler) -> Result<(), IcedError> {
+                let tmp = $scratch(&[dst.into(), self.into()]);
+                operation!(asm, tmp, { $add!(asm, dst, self, tmp) })
+            }
+            fn sub(self, dst: $dst, asm: &mut CodeAssembler) -> Result<(), IcedError> {
+                let tmp = $scratch(&[dst.into(), self.into()]);
+                operation!(asm, tmp, { $sub!(asm, dst, self, tmp) })
+            }
+        }
+        impl Obfuscate<$dst> for i32 {
+            fn add(self, dst: $dst, asm: &mut CodeAssembler) -> Result<(), IcedError> {
+                let tmp = $scratch(&[dst.into()]);
+                operation!(asm, tmp, {
+                    self.load(asm, tmp)?;
+                    $add!(asm, dst, tmp, tmp)
+                })
+            }
+            fn sub(self, dst: $dst, asm: &mut CodeAssembler) -> Result<(), IcedError> {
+                let tmp = $scratch(&[dst.into()]);
+                operation!(asm, tmp, {
+                    self.load(asm, tmp)?;
+                    $sub!(asm, dst, tmp, tmp)
+                })
+            }
+        }
+    };
 }
 
-impl Obfuscate<AsmRegister16> for AsmRegister16 {
-    fn add(self, dst: AsmRegister16, asm: &mut CodeAssembler) -> Result<(), IcedError> {
-        let tmp = scratch16(&[dst.into(), self.into()]);
-        mba_add!(asm, dst, self, tmp)
-    }
-
-    fn sub(self, dst: AsmRegister16, asm: &mut CodeAssembler) -> Result<(), IcedError> {
-        let tmp = scratch16(&[dst.into(), self.into()]);
-        mba_sub!(asm, dst, self, tmp)
-    }
-}
-
-impl Obfuscate<AsmRegister8> for AsmRegister8 {
-    fn add(self, dst: AsmRegister8, asm: &mut CodeAssembler) -> Result<(), IcedError> {
-        let tmp = scratch8(&[dst.into(), self.into()]);
-        mba_add!(asm, dst, self, tmp)
-    }
-
-    fn sub(self, dst: AsmRegister8, asm: &mut CodeAssembler) -> Result<(), IcedError> {
-        let tmp = scratch8(&[dst.into(), self.into()]);
-        mba_sub!(asm, dst, self, tmp)
-    }
-}
+impl_obfuscate!(AsmRegister64, scratch64, mba_add_reg, mba_sub_reg);
+impl_obfuscate!(AsmRegister32, scratch32, mba_add_reg, mba_sub_reg);
+impl_obfuscate!(AsmRegister16, scratch16, mba_add_reg, mba_sub_reg);
+impl_obfuscate!(AsmRegister8, scratch8, mba_add_reg, mba_sub_reg);
